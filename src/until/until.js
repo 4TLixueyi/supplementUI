2a545a868f97836ac20e5820c572a95f55cca9cf
lixueyi2019 <1310037427@qq.com>
2020-10-27 16:44:56 +0800

    20201027

--- src/until/until.js
+++ src/until/until.js
@@ -23,31 +23,59 @@ const TOOLS = {
         }
 
     },
+    //饼图 占比太小时，设置一个最小扇形（适用于移动端）
+    setSectorMinVal(data, type, minVal) {
+        type = type || 'value'
+        minVal = Number(minVal) || 0.03
+        data = JSON.parse(JSON.stringify(data))
+        let minArr = []
+        let maxArr = []
+        let maxArrTotalRate = 0
+        data.forEach(function (item) {
+            if (Number(item[type]) < minVal) {
+                item[type] = minVal
+                minArr.push(item)
+            } else {
+                maxArrTotalRate = maxArrTotalRate + Number(item[type])
+                maxArr.push(item)
+            }
+        });
+        let sumMinArr = minArr.length * minVal
+        // 如果有小于最小值时
+        if (sumMinArr) {
+            let sumMaxArr = 1 - sumMinArr
+            maxArr.forEach(function (item) {
+                item[type] = (item[type] / maxArrTotalRate) * sumMaxArr
+            })
+            data = maxArr.concat(minArr)
+        }
+        return data
+    },
     //饼图大小间隔排序
-    arrFormatSort(arr) {
-        let length = arr.length
-        let box = []
+    arrFormatSort(data) {
+        let length = data.length
+        let arr = []
         if (length && length > 3) {
             let ban = length / 2
             if (ban === Math.ceil(ban)) {
                 // 偶数个数时
                 for (let i = 0; i < ban; i++) {
-                    box.push(arr[i])
-                    box.push(arr[ban + i])
+                    arr.push(data[i])
+                    arr.push(data[ban + i])
                 }
             } else {
                 //奇数个数时
                 let ban2 = Math.floor(ban)
                 for (let i = 0; i < ban2; i++) {
-                    box.push(arr[i])
-                    box.push(arr[ban2 + 1 + i])
+                    arr.push(data[i])
+                    arr.push(data[ban2 + 1 + i])
                 }
-                box.push(arr[ban2 + 1])
+                arr.push(data[ban2 + 1])
             }
         } else {
-            box = arr
+            arr = data
         }
-        return box
+        return arr
     },
     // 四舍五入保留小数,callback
     toFixedFn(val, len, radix, callback) {
@@ -87,13 +115,28 @@ const TOOLS = {
                 return 0.1
             }
         } else {
+            num = String(num)
+            let arrAll = param.splite('.')
+            let arrLeft = arrAll[0].splite('')
+            let resStr = ''
             if (type === 'sec') {
-
+                // 第二位加1
+                for (let i = 0, len = arrLeft.length; i < len; i++) {
+                    if (i > 0) {
+                        resStr = resStr + '0'
+                    } else if (i == 1) {
+                        let sec = Number(arrLeft[1])
+                        if (sec !== 9) {
+                            resStr = resStr + (Number(arrLeft[1]) + 1)
+                        } else {
+                            resStr = Number(resStr) + 1 + '0'
+                        }
+                    } else {
+                        resStr = arrLeft[0]
+                    }
+                }
             } else {
-                num = String(num)
-                let arrAll = param.splite('.')
-                let arrLeft = arrAll[0].splite('')
-                let resStr = ''
+                // 第一位加1
                 for (let i = 0, len = arrLeft.length; i < len; i++) {
                     if (i !== 0) {
                         resStr = resStr + '0'
@@ -101,8 +144,9 @@ const TOOLS = {
                         resStr = arrLeft[0] + 1
                     }
                 }
-                return Number(resStr)
             }
+            return Number(resStr)
+
         }
     },
     //折线图Y轴 刻度值保留小数位
@@ -127,7 +171,29 @@ const TOOLS = {
         }
 
         return len
+    },
+    //markPotion 气泡
+    setMarkPotionArrow(data) {
+        let indexMax = data.length - 1
+        let obj = {}
+        if (indexMax > 3) {
+            obj = {
+                coord: [indexMax, data[indexMax]]
+            }
+        } else {
+            obj = {
+                coord: [indexMax, data[indexMax]],
+                symbol: 'image://static/img/xxx.png',
+                symbolOffset: ['50%', '-60%'],
+                label: {
+                    show: true,
+                    offset: [1, -1]
+                }
+            }
+        }
+        return obj
     }
+
 
 
 
